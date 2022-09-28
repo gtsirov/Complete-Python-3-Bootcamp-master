8adc45c50abd6556d2e0df79640113a29a57f67b
gtsirov <gtsirov@yahoo.com>
2022-09-28 11:47:58 +0100

    First working version of the game.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -11,7 +11,7 @@ def game_on():
         result = input("Would you like to play the Tic Tac Toe game? [Y or N]")
 
         if result not in ["Y", "N"]:
-            print("Input invalid, please try again...")
+            print("Input out of range, please try again...")
 
     return result == "Y"
 
@@ -25,24 +25,29 @@ def choose_symbol():
         result = input("Select your symbol? [X or O]")
 
         if result not in ["X", "O"]:
-            print("Input invalid, please try again...")
+            print("Input out of range, please try again...")
 
     return result
 
 
 def choose_board_position():
+    global board_map
     # Initialise variables
     result = "INVALID"
     board_positions = []
+    position_empty = False
     for i in range(1, 10):
         board_positions.append(str(i))
-    # Ask for user input
-    while result not in board_positions:
+    # Check if input is a number between 1 and 9
+    while not (result in board_positions and position_empty):
         result = input("Select a position on the board by using the keypad [1 to 9]")
-
         if result not in board_positions:
-            print("Input invalid, please try again...")
-
+            print("Input out of range, please try again...")
+        # Check if input position on the board is empty
+        elif board_map[result] != " ":
+            print("This board position is already taken, please try again...")
+        else:
+            position_empty = True
     return int(result)
 
 
@@ -59,10 +64,28 @@ def clear_board():
 
 
 def update_board(pos, sym):
-    pass
+    global board_map
+    board_map[str(pos)] = sym
+    # Check for a winner
+    if board_map["1"] == board_map["2"] == board_map["3"] == sym or \
+            board_map["4"] == board_map["5"] == board_map["6"] == sym or \
+            board_map["7"] == board_map["8"] == board_map["9"] == sym or \
+            board_map["1"] == board_map["4"] == board_map["7"] == sym or \
+            board_map["2"] == board_map["5"] == board_map["8"] == sym or \
+            board_map["3"] == board_map["6"] == board_map["9"] == sym or \
+            board_map["1"] == board_map["5"] == board_map["9"] == sym or \
+            board_map["3"] == board_map["5"] == board_map["7"] == sym:
+        #print("Winner!!!")
+        return "winner"
+    for k in board_map.keys():
+        if board_map[k] == " ":
+            return "continue"
+    else:
+        return "end"
 
 
 while game_on():
+    result = "play"
     clear_board()
     print("Player 1 will go first.")
     p1_symbol = choose_symbol()
@@ -72,6 +95,18 @@ while game_on():
         p2_symbol = "O"
     print(f"Player 1 symbol is {p1_symbol} and Player 2 symbol is {p2_symbol}.")
     display_board(board_map)
-    print(f"Player 1 it is your turn...")
-    p1_position = choose_board_position()
-    update_board(p1_position, p1_symbol)
+    while result != "end":
+        print(f"Player 1 it is your turn...")
+        p1_position = choose_board_position()
+        result = update_board(p1_position, p1_symbol)
+        display_board(board_map)
+        if result == "winner":
+            print(f"Player 1 won the game!")
+            break
+        print(f"Player 2 it is your turn...")
+        p2_position = choose_board_position()
+        result = update_board(p2_position, p2_symbol)
+        display_board(board_map)
+        if result == "winner":
+            print(f"Player 2 won the game!")
+            break
